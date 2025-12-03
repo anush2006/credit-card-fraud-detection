b4db1ac8b27090e65c0182657be2ded8f3c7c182
unknown <anush.nr@gmail.com>
2025-12-03 15:56:09 +0530

    EDA and SVM for baseline

---
+++ SVM.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+print("Loading dataset...")
+df = pd.read_csv('archive/creditcard.csv')
+print("Dataset loaded successfully.")
+"""Seperate the features and end target"""
+X=df.drop("Class",axis=1)
+y=df["Class"]
+print("Features and target variable separated.")
+""" Splitting the dataset into training and testing sets """
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+print("Dataset split into training and testing sets.")
+scaler = StandardScaler()
+""" Normalization of data """
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+"""Creating model"""
+# C=[1, 5, 10]
+#gamma =[0.01, 0.1, 1] test these values 
+svm_model = SVC(kernel='rbf',C=5,gamma=0.02,class_weight='balanced',random_state=42)
+"""Training the model"""
+print("Training the SVM model...")
+svm_model.fit(X_train_scaled, y_train)
+"""Making predictions"""
+print("Making predictions on the test set...")
+y_pred = svm_model.predict(X_test_scaled)
+"""Evaluating the model"""
+print("Evaluating the model...")
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred,digits=4))
---
+++ data_analysis.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+"""Load the credit card fraud detection dataset.
+"""
+df = pd.read_csv("archive/creditcard.csv")
+
+"""
+Find the number of rows and columns in the dataset.
+Display the first 5 rows of the dataset to have a reference.
+"""
+print(df.shape)
+print(df.head())
+"""
+Describe structural information about the dataset and statistical summary of numerical columns.
+(std,mean,min,max,25%/q1,50%/q2,75%/q3)
+"""
+print(df.info())
+print(df.describe())
+
+"""Check for missing values in the dataset.
+"""
+print(df.isnull().sum())
+
+"""
+Find class imbalance in the target variable 'Class'.
+"""
+print(df['Class'].value_counts())
+print(df['Class'].value_counts(normalize=True)*100)
+
+"""
+Visualize the class balance using a count plot.
+"""
+
+sns.countplot(data=df, x='Class')
+plt.title("Fraud vs Non-Fraud Count")
+plt.show()
+
+"""Visualize amount vs fraud using boxplot."""
+sns.boxplot(data=df, x='Class', y='Amount')
+plt.title("Amount vs Transaction Type")
+plt.show()
+
+
+"""Correlation analysis between features and target variable."""
+plt.figure(figsize=(12,6))
+sns.heatmap(df.corr(), cmap='coolwarm', linewidths=0.1)
+plt.title("Correlation Heatmap")
+plt.show()
+
+"""PCA visualization of the first two principal components."""
+
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(df.drop("Class", axis=1))
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+
+plt.scatter(X_pca[:,0], X_pca[:,1], c=df['Class'], cmap='coolwarm', s=2)#Scatter plot for PCA components
+plt.title("PCA Visualization of Fraud Separation")
+plt.show()
